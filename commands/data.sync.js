377183cc9ac5714bdd839c30e8f5ba1bf49abee6
goliatone <burgosemi@gmail.com>
2017-05-26 18:52:05 -0400

    Update data.sync command to solve printing cyclical reference error

--- commands/data.sync.js
+++ commands/data.sync.js
@@ -12,14 +12,14 @@ const exists = require('fs-exists-promised');
 module.exports = function dataSync(event){
     var app = this;
 
-    app.getLogger('data.sync').info('dataSync: %s', JSON.stringify(event, null, 4));
+    app.getLogger('data.sync').info('dataSync: %s', Object.keys(event));
 
-    if(!event.entity){
+    if(!event.entity) {
         app.getLogger('data.sync').warn('Ignoring event, we dont have valid "entity"');
         return;
     }
 
-    if(!event.filepath){
+    if(!event.filepath) {
         app.getLogger('data.sync').warn('Ignoring event, we dont have valid "filepath"');
         return;
     }
@@ -27,7 +27,6 @@ module.exports = function dataSync(event){
     //TODO: we could validate event.filepath and a) make sure exists b) it's valid type.
 
     var moveAfterDone = app.config.get('filesync.' + event.origin + '.moveAfterDone', false);
-
 
     app.datamanager.importFileAsModels(event.entity, event.filepath).then((records)=>{
         app.getLogger('data.sync').info('sync completed for entity %s', event.entity);
